b08a685d76ef30ab003357ecb3938dc09e965f19
tGodson <tendongze95@gmail.com>
2021-08-23 20:53:25 +0100

    install chonky test it out with demo code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { setChonkyDefaults } from 'chonky';
+import { ChonkyIconFA } from 'chonky-icon-fontawesome';
+import { FullFileBrowser } from 'chonky';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+setChonkyDefaults({ iconComponent: ChonkyIconFA });
+
+const App = () => {
+  const files = [
+    { id: 'lht', name: 'Projects', isDir: true },
+    {
+        id: 'mcd',
+        name: 'chonky-sphere-v2.png',
+        thumbnailUrl: 'https://chonky.io/chonky-sphere-v2.png',
+    },
+];
+const folderChain = [{ id: 'xcv', name: 'Demo', isDir: true }];
+return (
+    <div style={{ height: 300 }}>
+        <FullFileBrowser files={files} folderChain={folderChain} />
     </div>
-  );
+);
 }
 
 export default App;
